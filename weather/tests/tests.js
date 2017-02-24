aa886b62a1ac26805f551d024ed0c8c7ebb19cee
Adriana <adriana.rosa@devinci.fr>
2017-02-24 19:07:35 +0100

    comments: try to understand the mecanism of the test

--- weather/tests/tests.js
+++ weather/tests/tests.js
@@ -5,7 +5,18 @@ chai.should();
 
 const weather = require('../index.js');
 
+//asynchronous code
 describe('Weather', () => {
 	it('should get the weather', (done) => {
 	});
 });
+
+
+/**
+ * describe('nameofthefunction', function(){
+ * it('should done this', function(done){
+ * 	set the variable
+ *	test your function
+ *  });
+ * });
+**/
